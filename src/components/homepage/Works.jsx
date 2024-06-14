1c1e82806bf4280cd2af41a979b0d623a6290616
Sumeru Das <sumerudas2019@gmail.com>
2024-06-14 17:24:20 +0530

    added project section

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -1,73 +1,80 @@
+import coindome from "../../assets/images/coindome.webp";
+import dash from "../../assets/images/dash.webp";
+import moqo from "../../assets/images/moqo.webp";
+import rejoice from "../../assets/images/rejoice.webp";
+import digital from "../../assets/images/digital.webp";
+
+import Projects from "../ui/Projects";
 import TextMask from "../ui/TextMask";
 
-import { HoverEffect } from "../ui/card-hover-effect";
-
-// export default function Works({ forwardedRef }) {
-//   const phrases = ["Projects"];
-
-//   return (
-//     <section
-//       ref={forwardedRef}
-//       id="works"
-//       className="nav-change mt-20 h-screen overflow-hidden px-10"
-//     >
-//       <h1 className="mt-10 text-[5vw] font-semibold uppercase text-[#FFFFFF] sm:my-20 sm:text-[3vw]">
-//         <TextMask>{phrases}</TextMask>
-//       </h1>
-//     </section>
-//   );
-// }
-
 export default function Works({ forwardedRef }) {
-  const phrases = ["Projects"];
-
-  const projects = [
-    {
-      title: "Stripe",
-      description:
-        "A technology company that builds economic infrastructure for the internet.",
-      link: "https://stripe.com",
-    },
-    {
-      title: "Netflix",
-      description:
-        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-      link: "https://netflix.com",
-    },
-    {
-      title: "Google",
-      description:
-        "A multinational technology company that specializes in Internet-related services and products.",
-      link: "https://google.com",
-    },
-    {
-      title: "Meta",
-      description:
-        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-      link: "https://meta.com",
-    },
-    {
-      title: "Amazon",
-      description:
-        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-      link: "https://amazon.com",
-    },
-    {
-      title: "Microsoft",
-      description:
-        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-      link: "https://microsoft.com",
-    },
-  ];
-
+  const phrases = ["About"];
   return (
-    <section ref={forwardedRef} className="h-screen ">
-      <div className="flex h-full flex-col items-center gap-10  px-10">
-        <h1 className="mt-10 text-[5vw] font-semibold uppercase text-[#FFFFFF] sm:my-20 sm:text-[3vw]">
-          <TextMask>{phrases}</TextMask>
-        </h1>
-        <div className="flex w-[80%] items-center justify-center overflow-y-scroll sm:overflow-y-hidden">
-          <HoverEffect items={projects} />
+    <section
+      ref={forwardedRef}
+      id="works"
+      className="nav-change my-[10%] overflow-hidden"
+    >
+      <h1 className="flex items-center justify-center text-[5vw] font-semibold uppercase text-[#FFFFFF] sm:my-20 sm:text-[3vw]">
+        <TextMask>{phrases}</TextMask>
+      </h1>
+      <div className="mt-10 grid cursor-pointer grid-cols-1 gap-16 gap-y-10 p-5 md:grid-cols-12">
+        {/* Project #1 */}
+        <div className=" col-span-1 md:col-span-12">
+          <Projects
+            link="https://coin-dome.vercel.app/"
+            img={coindome}
+            alt="Coindome"
+            name="Coindome | a crypto tracking website"
+            type="API Integration • Frontend Development"
+            year="2024"
+            tools="Reactjs • Framer Motion • Coingecko API • Chart.js"
+          />
+        </div>
+        {/* Project #2 */}
+        <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
+          <Projects
+            link="https://dash-ivory-three.vercel.app/"
+            img={dash}
+            alt="Dash"
+            name="Dash | a video conferencing website"
+            type="Full-Stack Development Development"
+            year="2024"
+            tools="TypeScript • Nextjs • Tailwind CSS • Stream SDK • Clerk"
+          />
+        </div>
+        <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
+          <Projects
+            link="https://moqo-moqo.vercel.app/"
+            img={moqo}
+            alt="MOQO"
+            name="MOQO | Mordern Web Design"
+            type="Web Design • Frontend Development"
+            year="2023"
+            tools="HTML • CSS • JavaScript • GSAP|ScrollTrigger"
+          />
+        </div>
+        <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
+          <Projects
+            link="https://rejoice-brown.vercel.app/"
+            img={rejoice}
+            alt="Rejoice"
+            name="Rejoice Website"
+            type="Frontend Development • Web Design"
+            year="2023"
+            tools="HTML • CSS • JavaScript • GSAP|ScrollTrigger • LocomotiveScroll"
+          />
+        </div>
+        <div className="col-span-1 h-fit md:col-span-4">
+          <Projects
+            link="https://digital-phi-one.vercel.app/"
+            img={digital}
+            alt="Digital Performance Solution"
+            name="Digital Performance Solution"
+            type="Frontend Development"
+            year="2023"
+            tools="HTML • CSS • JavaScript • GSAP|ScrollTrigger"
+          />
         </div>
       </div>
     </section>
